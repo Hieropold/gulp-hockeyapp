2df7da4943a37a31631b64251b6816252bb4aa23
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2015-12-15 13:26:26 +0300

    More input options, list of teams.

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ var request = require('request');
 
 module.exports = {
   upload: function(options) {
+
+    var postOpts = {
+      url: 'https://rink.hockeyapp.net/api/2/apps/' + options.id + '/app_versions/upload',
+      headers: {
+        'X-HockeyAppToken': options.apiToken
+      },
+      formData: {
+        ipa: fs.createReadStream(options.inputFile),
+        notify: options.notify,
+        status: options.status
+      }
+    };
+
+    if (options.teamList && options.teamList.length) {
+      postOpts.formData.teams = options.teamList.join(',');
+    }
+
     return new Promise(function(resolve, reject) {
-      var postOpts = {
-        url: 'https://rink.hockeyapp.net/api/2/apps/' + options.id + '/app_versions/upload',
-        headers: {
-          'X-HockeyAppToken': options.apiToken
-        },
-        formData: {
-          ipa: fs.createReadStream(options.inputFile),
-          notify: 0,
-          status: 2,
-          teams: options.teamList
-        }
-      };
 
       request.post(postOpts, function(err, response) {
         if (err) {
